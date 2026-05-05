1c323e7bc5146f7d1ee76b628f9895eb8e406252
bsanthosh-dev <bsanthosh2793@gmail.com>
2026-05-05 21:50:15 +0530

    Update ExpenseiqApplication.java

--- src/main/java/com/Santhosh/expenseiq/ExpenseiqApplication.java
+++ src/main/java/com/Santhosh/expenseiq/ExpenseiqApplication.java
@@ -3,7 +3,7 @@ package com.santhosh.expenseiq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.santhosh")
+@SpringBootApplication
 public class ExpenseiqApplication {
 
     public static void main(String[] args) {
